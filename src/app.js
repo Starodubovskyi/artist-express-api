c7ccf0e76f3026cc1cdefa19bf4ca291c17eae9f
Starodubovskyi <hard.guzanu@gmail.com>
2022-12-08 15:49:21 +0200

    update

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const compression = require('compression');
 const cors = require('cors');
 const passport = require('passport');
 const httpStatus = require('http-status');
+const path = require('path');
 const config = require('./config/config');
 const morgan = require('./config/morgan');
 const { jwtStrategy } = require('./config/passport');
@@ -20,7 +21,8 @@ if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
-app.use(express.static(`${__dirname}/Images/`));
+app.use(express.static(`${__dirname}/images/`));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // set security HTTP headers
 app.use(helmet());
