a744fb79a9f6e1252bd1cec06a427cfa870b468c
Starodubovskyi <hard.guzanu@gmail.com>
2022-12-10 11:26:49 +0200

    update image

--- src/app.js
+++ src/app.js
@@ -21,8 +21,8 @@ if (config.env !== 'test') {
   app.use(morgan.successHandler);
   app.use(morgan.errorHandler);
 }
-// app.use(express.static(`${__dirname}/images/`));
-app.use('/images/', express.static(path.join(__dirname, 'images')));
+app.use(express.static(`${__dirname}/images/`));
+// app.use('/images/', express.static(path.join(__dirname, 'images')));
 
 // set security HTTP headers
 app.use(helmet());
